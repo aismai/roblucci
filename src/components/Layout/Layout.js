c6e7c95275d864a5fa4b96ad1758b02519ff4c3c
aismai <abai.ismailov@gmail.com>
2018-08-20 14:28:09 +0600

    add BurgerIngridient component

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,10 +1,12 @@
 import React from "react";
 import Aux from "../../hoc/Aux";
 
+import styles from "./Layout.css";
+
 const layout = props => (
   <Aux>
     <div>Toolbar, Sidedrawer, Backdrop</div>
-    <main>{props.children}</main>
+    <main className={styles.Content}>{props.children}</main>
   </Aux>
 );
 
